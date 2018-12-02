43a28820cf94c07561cb0724c17db3bf77c5e4eb
Bruno Silverio <brunocsilverio@hotmail.com>
2018-12-02 14:57:58 -0200

    Update Automovel.java

--- src/com/iaglourenco/Automovel.java
+++ src/com/iaglourenco/Automovel.java
@@ -1,24 +1,17 @@
 package com.iaglourenco;
 
 public class Automovel {
-    public static final int CAMINHONETE=0,CARRO=1,MOTO=2;
+    static final int CAMINHONETE=0,CARRO=1,MOTO=2;
 
 
-    private String marca;
-    private String modelo;
     private String placa;
     private int tipo;
 
-    public Automovel(String marca,String modelo,String placa,int tipo){
-       this.marca=marca;
-       this.modelo=modelo;
+    public Automovel(String placa,int tipo){
        this.placa=placa;
        this.tipo=tipo;
-
     }
 
-    String getMarca(){return this.marca;}
-    String getModelo(){return this.modelo;}
     String getPlaca(){return this.placa;}
     int getTipo(){return this.tipo;}
 
